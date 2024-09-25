b94d19ab737d364026e65559e9d17d7798e53bba
Krishna Prasad <76819369+bharathulaprasad@users.noreply.github.com>
2024-09-25 11:26:50 +0200

    Update test.sql

--- test.sql
+++ test.sql
@@ -2,6 +2,7 @@ SET SERVEROUTPUT ON SIZE 10000000000;
 BEGIN                                      
    find_strings_in_tables('something%,%moreonsomething_','VARCHAR2')  ;      
    find_string_in_tables('something','VARCHAR2')  ;   
+   find_and_replace_strings_in_tables ('something','somethingelse')  ;  
    --commit;
 END;
 
